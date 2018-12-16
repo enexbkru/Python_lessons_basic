23185a92541dc7ef31de2364e3637d82fa6b63a7
enexbkru <45367119+enexbkru@users.noreply.github.com>
2018-12-17 01:41:46 +0700

    ДЗ 6 урок
    Долго провозился с простыми вещами. 
    Уверен, что разберусь, но пока не хватает опыта и времени.
    Hard не успеваю

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -1,9 +1,111 @@
+__author__ = 'Кирьянов Евгений Александрович'
+
 # Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
+
+import math
+
+print('Залдача 1')
+
+class Triangle:
+    def __init__(self, A, B, C):
+
+        self.A = A
+        self.B = B
+        self.C = C
+
+        # длина стороны
+        def side(a, b):
+            return math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
+
+        self.AB = side(A, B)
+        self.BC = side(B, C)
+        self.CA = side(C, A)
+
+
+    # метод для площади
+    def S(self):
+
+        #полупериметр
+        hp = self.P() / 2
+
+        # площадь
+        return math.sqrt(hp * (hp - self.AB) * (hp - self.BC) * (hp - self.CA))
+
+    # метод периметра
+    def P(self):
+        return self.AB + self.BC + self.CA
+
+    # метод высоты
+    def H(self):
+        return self.S() / (self.AB / 2)
+
+
+t_1 = Triangle((0, 0), (0, 4), (4, 0))
+
+print('Сторона А: ', t_1.AB)
+print('Сторона B: ', t_1.BC)
+print('Сторона C: ', t_1.CA)
+print('Площадь: ', t_1.S())
+print('Высота: ', t_1.H())
+print('Периметр: ', t_1.P())
 
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
 # Предусмотреть в классе методы:
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
+
+print('Задача 2')
+
+class Trapeze:
+    def __init__(self, A, B, C, D):
+
+        self.A = A
+        self.B = B
+        self.C = C
+        self.D = D
+
+        # длина стороны
+        def side(a, b):
+            return math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
+
+        self.AB = side(A, B)
+        self.BC = side(B, C)
+        self.CD = side(C, D)
+        self.DA = side(D, A)
+
+        # вариант площади через площадь двух составляющих ее треугольников
+        def S(a, b, c):
+            hp = (a + b + c) / 2
+            return math.sqrt(hp * (hp - a) * (hp - b) * (hp - c))
+
+        self.D_AC = side(self.C, self.A)
+        self.D_BD = side(self.B, self.D)
+        self.P = self.AB + self.BC + self.CD + self.DA
+
+        self.S = S(self.AB, self.D_BD, self.DA) + S(self.D_BD, self.BC, self.CD)
+
+        # вариант площади по четырем сторонам
+        def S1(a, b, c, d):
+            return (a + b) / 2 * math.sqrt(c**2 - (((a - b)**2 + c**2 - d**2) / 2 * (a - b))**2)
+        # для равнобедренной можно еще через высоту, тут не привожу, времнеи не хватит
+        self.S1 = S1
+
+
+Trapeze_1 = Trapeze((0, 0), (10, 0), (8, 4), (2, 4))
+
+if Trapeze_1.D_AC == Trapeze_1.D_BD and Trapeze_1.BC == Trapeze_1.DA:
+    print("Трапеция равнобедренная")
+else:
+    print("Трапеция НЕ равнобедренная")
+
+print('Сторона A: ', Trapeze_1.AB)
+print('Сторона B: ', Trapeze_1.BC)
+print('Сторона C: ', Trapeze_1.CD)
+print('Сторона D: ', Trapeze_1.DA)
+print('Периметр: ', Trapeze_1.P)
+print('Площадь: ', Trapeze_1.S)
+print('Площадь1: ', Trapeze_1.S1)
+
 
--- lesson06/home_work/hw06_normal.py
+++ lesson06/home_work/hw06_normal.py
@@ -1,3 +1,6 @@
+__author__ = 'Кирьянов Евгений Александрович'
+
+print('ЗАдача 1')
 # Задание-1:
 # Реализуйте описаную ниже задачу, используя парадигмы ООП:
 # В школе есть Классы(5А, 7Б и т.д.), в которых учатся Ученики.
@@ -15,3 +18,95 @@
 #  (Ученик --> Класс --> Учителя --> Предметы)
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
+
+class ClassRoom:
+    def __init__(self, class_room):
+        self.class_room = class_room
+
+class Person:
+    def __init__(self, name, surname, father_name):
+        self.name = name
+        self.surname = surname
+        self.father_name = father_name
+
+    def get_ini_name(self):
+        return '{} {}.{}.'.format(self.surname.title(), self.name[0].upper(), self.father_name[0].upper())
+
+
+class Student(Person):
+    def __init__(self, name, surname, father_name, class_room, father, mother):
+        Person.__init__(self, name, surname, father_name)
+        self.class_room = class_room
+        self.father = father
+        self.mother = mother
+
+    def get_class_room(self):
+        return self.class_room
+
+    def get_parents(self):
+        return self.father.get_ini_name(), self.mother.get_ini_name()
+
+class Teacher(Person):
+    def __init__(self, name, surname, father_name, classes, subject):
+        Person.__init__(self, name, surname, father_name)
+        self.classes = classes
+        self.subject = subject
+
+    def get_subject(self):
+        return self.subject
+
+    def get_classes(self):
+        return self.classes
+
+# задаем данные
+
+
+class_rooms = ['5А', '6А', '7А', '8А', '9А']
+
+parents = [Person("Иван", "Петров", "Александрович"),
+           Person("Татьяна", "Петрова", "Александровна"),
+           Person("Игорь", "Сидоров", "Александрович"),
+           Person("Оксана", "Сидорова", "Александровна"),
+           Person("Виталий", "Иванов", "Александрович"),
+           Person("Милана", "Иванова", "Александровна")]
+
+students = [Student("Александр", "Иванов", "Витальевич", class_rooms[2], parents[4], parents[5]),
+            Student("Александра", "Иванова", "Витальевна", class_rooms[0], parents[4], parents[5]),
+            Student("Петр", "Сидоров", 'Игоревич', class_rooms[1], parents[2], parents[3]),
+            Student("Октябрина", "Сидорова", 'Игоревна', class_rooms[3], parents[2], parents[3]),
+            Student("Иван", "Петров", 'Иванович', class_rooms[4], parents[0], parents[1]),
+            Student("Илона", "Петрова", 'Ивановна', class_rooms[2], parents[0], parents[1])]
+
+teachers = [Teacher("Феофан", "Грек", "Батькович", [class_rooms[0], class_rooms[1]], 'Русский язык'),
+            Teacher("Андрей", "Зырянов", "Александрович", [class_rooms[2], class_rooms[1]], 'История'),
+            Teacher("Ульянв", "Харитонова", "Яковлевна", [class_rooms[3], class_rooms[4]], 'Литература')]
+
+        # 1. Получить полный список всех классов школы
+print('Полный список всех классов школы: ')
+print('class_rooms ', class_rooms)
+
+        # 2. Получить список всех учеников в указанном классе
+        #  (каждый ученик отображается в формате "Фамилия И.О.")
+print("Список всех учеников в указанном классе: ")
+for num, student in enumerate(students, start=1):
+    print("{}) {} класс: {}".format(num, student.get_ini_name(), student.class_room))
+
+    # 3. Получить список всех предметов указанного ученика
+    #  (Ученик --> Класс --> Учителя --> Предметы)
+
+student = students[0]
+t_list = [val for val in teachers if student.get_class_room() in val.get_classes()]
+t_names = [val.get_ini_name() for val in t_list]
+subj = [val.get_subject() for val in teachers]
+print(student.get_ini_name() + ' --> ' + student.get_class_room() + ' --> ' + ' '.join(map(str, t_names)) + '--> ' + ' '.join(map(str, subj)))
+
+    # 4. Узнать ФИО родителей указанного ученика
+st_parents = student.get_parents()
+print('Родители ученика ', student.get_ini_name(), ' ', st_parents)
+
+    # 5. Получить список всех Учителей, преподающих в указанном классе
+print('Учителя, преподающие в классе:')
+for num, teacher in enumerate(teachers, start=1):
+    print("{}) {} класс: {}".format(num, teacher.get_ini_name(), teacher.classes))
+
+
